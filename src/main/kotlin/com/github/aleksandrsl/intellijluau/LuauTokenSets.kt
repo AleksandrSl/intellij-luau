290db86f33329610fdeba9c75117afda2ac7d8bc
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2024-05-18 22:37:06 +0200

    fix: template literal string quote handling

--- src/main/kotlin/com/github/aleksandrsl/intellijluau/LuauTokenSets.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijluau/LuauTokenSets.kt
@@ -28,6 +28,9 @@ object LuauTokenSets {
         // Luau type cast operator
         LuauTypes.DOUBLE_COLON,
     )
-    // TODO: Template string doesn't give any effect. Have to fix this, need to look at a language parser implementation with such strings
-    val STRINGS = TokenSet.create(LuauTypes.STRING, LuauTypes.TEMPLATE_STRING)
+    // Quote handler expects all symbols that represent strings' quotes to be present.
+    // I have zero idea how it decides which one is opening and which one is closing if I'm just passing the set.
+    // Probably it just makes a decision based on the presence of any token from the set before?
+    // But it also should know what is the closed string, whatever it works ok so far.
+    val STRINGS = TokenSet.create(LuauTypes.STRING, LuauTypes.TEMPLATE_STRING_SQUOTE, LuauTypes.TEMPLATE_STRING_EQUOTE)
 }
