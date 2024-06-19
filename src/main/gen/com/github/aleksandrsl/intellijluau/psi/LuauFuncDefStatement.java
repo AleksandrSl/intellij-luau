69148db4fb629b9daaba8a1befa5b30c4fa11416
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2024-06-20 00:06:34 +0200

    fix: add missing generated parser classes

---
+++ src/main/gen/com/github/aleksandrsl/intellijluau/psi/LuauFuncDefStatement.java
@@ -0,0 +1,20 @@
+// This is a generated file. Not intended for manual editing.
+package com.github.aleksandrsl.intellijluau.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+import com.intellij.navigation.ItemPresentation;
+
+public interface LuauFuncDefStatement extends PsiElement {
+
+  @NotNull
+  LuauFuncBody getFuncBody();
+
+  @NotNull
+  LuauFuncName getFuncName();
+
+  @NotNull
+  ItemPresentation getPresentation();
+
+}
---
+++ src/main/gen/com/github/aleksandrsl/intellijluau/psi/impl/LuauFuncDefStatementImpl.java
@@ -0,0 +1,49 @@
+// This is a generated file. Not intended for manual editing.
+package com.github.aleksandrsl.intellijluau.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.github.aleksandrsl.intellijluau.psi.LuauTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.github.aleksandrsl.intellijluau.psi.*;
+import com.intellij.navigation.ItemPresentation;
+
+public class LuauFuncDefStatementImpl extends ASTWrapperPsiElement implements LuauFuncDefStatement {
+
+  public LuauFuncDefStatementImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull LuauVisitor visitor) {
+    visitor.visitFuncDefStatement(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof LuauVisitor) accept((LuauVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public LuauFuncBody getFuncBody() {
+    return findNotNullChildByClass(LuauFuncBody.class);
+  }
+
+  @Override
+  @NotNull
+  public LuauFuncName getFuncName() {
+    return findNotNullChildByClass(LuauFuncName.class);
+  }
+
+  @Override
+  @NotNull
+  public ItemPresentation getPresentation() {
+    return LuauPsiImplUtilKt.getPresentation(this);
+  }
+
+}
