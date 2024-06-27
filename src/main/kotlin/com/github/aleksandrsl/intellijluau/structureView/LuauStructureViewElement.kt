f6a1a2a3713e2c61cdaf34efb384fb5cb70b4b4b
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2024-06-27 12:19:51 +0200

    feat: group methods under the variable they are tied to in structure view

--- src/main/kotlin/com/github/aleksandrsl/intellijluau/structureView/LuauStructureViewElement.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijluau/structureView/LuauStructureViewElement.kt
@@ -1,43 +1,65 @@
 package com.github.aleksandrsl.intellijluau.structureView
 
-import com.github.aleksandrsl.intellijluau.psi.LuauFile
-import com.github.aleksandrsl.intellijluau.psi.LuauFuncDefStatement
-import com.github.aleksandrsl.intellijluau.psi.LuauLocalDefStatement
-import com.github.aleksandrsl.intellijluau.psi.LuauLocalFuncDefStatement
-import com.github.aleksandrsl.intellijluau.psi.LuauMethodDefStatement
+import com.github.aleksandrsl.intellijluau.psi.*
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.structureView.StructureViewTreeElement
 import com.intellij.ide.util.treeView.smartTree.TreeElement
 import com.intellij.navigation.ItemPresentation
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.psi.NavigatablePsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.containers.map2Array
 
+private val LOG = logger<LuauStructureViewElement>()
 
-class LuauStructureViewElement(private val myElement: NavigatablePsiElement) : StructureViewTreeElement {
+class LuauStructureViewElement(private val myElement: NavigatablePsiElement, val context: Context? = null) :
+    StructureViewTreeElement {
     override fun getPresentation(): ItemPresentation {
         val presentation = myElement.presentation
         return presentation ?: PresentationData().apply { presentableText = "Missing presentation" }
     }
 
     override fun getChildren(): Array<out TreeElement> {
-        if (myElement is LuauFile) {
-            return PsiTreeUtil
-                .findChildrenOfAnyType(
-                    myElement,
-                    LuauFuncDefStatement::class.java,
-                    LuauLocalFuncDefStatement::class.java,
-                    LuauMethodDefStatement::class.java,
-                )
-                .map2Array { LuauStructureViewElement(it as NavigatablePsiElement) }.plus(PsiTreeUtil
+        when (myElement) {
+            is LuauFile -> {
+                val methods = PsiTreeUtil
+                    .findChildrenOfType(
+                        myElement,
+                        LuauMethodDefStatement::class.java,
+                    )
+                return PsiTreeUtil
                     .findChildrenOfAnyType(
                         myElement,
-                        LuauLocalDefStatement::class.java,
+                        LuauFuncDefStatement::class.java,
+                        LuauLocalFuncDefStatement::class.java,
                     )
-                    .mapNotNull { it.bindingList?.bindingList }.flatten()
-                    .map2Array { LuauStructureViewElement(it as NavigatablePsiElement) })
+                    .map2Array { LuauStructureViewElement(it as NavigatablePsiElement) }.plus(PsiTreeUtil
+                        .findChildrenOfAnyType(
+                            myElement,
+                            LuauLocalDefStatement::class.java,
+                        )
+                        .mapNotNull { it.bindingList?.bindingList }.flatten()
+                        .map2Array { LuauStructureViewElement(it as NavigatablePsiElement, Context(methods)) })
+            }
+
+            is LuauBinding -> {
+                // TODO (AleksandrSl 27/06/2024): 1. Write tests
+                //  2. Handle methods added to variables defined somewhere else?
+                //  I guess I need to create a map of variable to methods beforehand,
+                //  because there is no strict hierarchy between them
+                //  3. Handle duplicate declarations and their children correctly.
+                //  Should we just keep one, or both?
+                //  First exists and can be used until the second one is defined.
+                //  4. How can I provide different presentation for a method that's a children of another element?
+                return context?.methods?.filter {
+                    val match = Regex("(.*)[.:][^.:]+").matchEntire(it.methodName.text)
+                    match != null && match.groups[1]?.value == myElement.id.text
+                }
+                    ?.map2Array { LuauStructureViewElement(it as NavigatablePsiElement) } ?: TreeElement.EMPTY_ARRAY
+            }
+
+            else -> return TreeElement.EMPTY_ARRAY
         }
-        return TreeElement.EMPTY_ARRAY
     }
 
     override fun getValue(): NavigatablePsiElement {
@@ -56,3 +78,5 @@ class LuauStructureViewElement(private val myElement: NavigatablePsiElement) : S
         return myElement.navigate(requestFocus)
     }
 }
+
+class Context(val methods: Collection<LuauMethodDefStatement>)
