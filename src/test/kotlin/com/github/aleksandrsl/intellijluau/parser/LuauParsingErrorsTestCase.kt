23cc2620f66ac78f75922f2730ed299de1d7141c
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-03-07 00:18:15 +0100

    feat: add more tests for AlgebraicTypes

--- src/test/kotlin/com/github/aleksandrsl/intellijluau/parser/LuauParsingErrorsTestCase.kt
+++ src/test/kotlin/com/github/aleksandrsl/intellijluau/parser/LuauParsingErrorsTestCase.kt
@@ -7,6 +7,7 @@ class LuauParsingErrorsTestCase : LuauParsingBaseTestCase() {
     fun testComplexGenericDefaults_error() = doTest(true)
     fun testAlgebraicTypes_error() = doTest(true)
     fun testAlgebraicTypes_error2() = doTest(true)
+    fun testAlgebraicTypes_error3() = doTest(true)
     fun testComplexTypes_error() = doTest(true)
     fun testStringTemplate_error() = doTest(true)
 
