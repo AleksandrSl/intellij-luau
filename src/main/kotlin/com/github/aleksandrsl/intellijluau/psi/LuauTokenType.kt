d8e24a6f6dd872a4a940d8bd6719226aefde6976
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-03-28 11:06:14 +0100

    feat: drop prefix in token debug name
    I don't know why they use it in the example, but plugins are not doing that.

--- src/main/kotlin/com/github/aleksandrsl/intellijluau/psi/LuauTokenType.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijluau/psi/LuauTokenType.kt
@@ -3,8 +3,4 @@ package com.github.aleksandrsl.intellijluau.psi
 import com.github.aleksandrsl.intellijluau.LuauLanguage
 import com.intellij.psi.tree.IElementType
 
-class LuauTokenType(debugName: String) : IElementType(debugName, LuauLanguage.INSTANCE) {
-    override fun toString(): String {
-        return "LuauTokenType.${super.toString()}"
-    }
-}
+class LuauTokenType(debugName: String) : IElementType(debugName, LuauLanguage.INSTANCE)
