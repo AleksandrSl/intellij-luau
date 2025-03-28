235667d8b72781df63025378d1382c5e60b805ec
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-03-28 15:52:47 +0100

    chore: tests for recovery

--- src/test/kotlin/com/github/aleksandrsl/intellijluau/parser/LuauParsingBaseTestCase.kt
+++ src/test/kotlin/com/github/aleksandrsl/intellijluau/parser/LuauParsingBaseTestCase.kt
@@ -2,15 +2,21 @@ package com.github.aleksandrsl.intellijluau.parser
 
 import com.github.aleksandrsl.intellijluau.LuauFileType
 import com.github.aleksandrsl.intellijluau.LuauParserDefinition
+import com.github.aleksandrsl.intellijluau.psi.LuauListArgs
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiErrorElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.findParentOfType
+import com.intellij.psi.util.parents
+import com.intellij.psi.util.parentsOfType
 import com.intellij.testFramework.ParsingTestCase
+import io.ktor.util.reflect.*
 
 const val baseTestDataPath = "src/test/testData/parser"
 
-open class LuauParsingBaseTestCase : ParsingTestCase("", LuauFileType.defaultExtension, LuauParserDefinition()) {
+open class LuauParsingBaseTestCase(path: String = "") :
+    ParsingTestCase(path, LuauFileType.defaultExtension, LuauParserDefinition()) {
     protected fun hasErrors(file: PsiFile): Boolean {
         var hasErrors = false
         file.accept(object : PsiElementVisitor() {
