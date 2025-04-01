829ccf39ec1ef683d0f3fb8f4b5af9f62e5e7509
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-04-01 16:10:31 +0200

    fix: func body folding
    Replace func body with blocks, should be good to fold if else blocks as well. Root block has a different type, so it can't be folded and that's good.
    https://github.com/AleksandrSl/intellij-luau/issues/72

--- src/main/kotlin/com/github/aleksandrsl/intellijluau/LuauFoldingBuilder.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijluau/LuauFoldingBuilder.kt
@@ -1,6 +1,5 @@
 package com.github.aleksandrsl.intellijluau
 
-import com.github.aleksandrsl.intellijluau.psi.LuauFuncBody
 import com.github.aleksandrsl.intellijluau.psi.LuauTableConstructor
 import com.github.aleksandrsl.intellijluau.psi.LuauTypes
 import com.intellij.lang.ASTNode
@@ -11,9 +10,8 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiRecursiveElementVisitor
-import com.intellij.psi.util.endOffset
 
-class LuauFoldingBuilder: FoldingBuilderEx(), DumbAware {
+class LuauFoldingBuilder : FoldingBuilderEx(), DumbAware {
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
 
         val list = mutableListOf<FoldingDescriptor>()
@@ -21,16 +19,37 @@ class LuauFoldingBuilder: FoldingBuilderEx(), DumbAware {
         root.accept(object : PsiRecursiveElementVisitor() {
             override fun visitElement(element: PsiElement) {
                 val node = element.node
-                when(node.elementType) {
-                    LuauTypes.FUNC_BODY -> {
-                        // TODO (AleksandrSl 23/05/2024): I don't include ) in parameters list. I think I'll change the grammar at some point.
-                        list.add(FoldingDescriptor(node, TextRange((element as LuauFuncBody).parList?.endOffset?.inc() ?: node.startOffset, node.startOffset + node.textLength)))
+                when (node.elementType) {
+                    LuauTypes.BLOCK -> {
+                        val prev = node.treePrev
+                        val next = node.treeNext
+                        if (prev != null && next != null) {
+                            val l = prev.startOffset + prev.textLength
+                            val r = next.startOffset
+
+                            val range = TextRange(l, r)
+                            if (range.length > 0) {
+                                list.add(FoldingDescriptor(element, range))
+                            }
+                        }
                     }
+
                     LuauTypes.TABLE_CONSTRUCTOR -> {
-                        list.add(FoldingDescriptor(node, TextRange(node.startOffset, node.startOffset + node.textLength)))
+                        list.add(
+                            FoldingDescriptor(
+                                node,
+                                TextRange(node.startOffset, node.startOffset + node.textLength)
+                            )
+                        )
                     }
+
                     LuauTypes.BLOCK_COMMENT -> {
-                        list.add(FoldingDescriptor(node, TextRange(node.startOffset, node.startOffset + node.textLength)))
+                        list.add(
+                            FoldingDescriptor(
+                                node,
+                                TextRange(node.startOffset, node.startOffset + node.textLength)
+                            )
+                        )
                     }
                 }
                 super.visitElement(element)
@@ -40,9 +59,9 @@ class LuauFoldingBuilder: FoldingBuilderEx(), DumbAware {
     }
 
     override fun getPlaceholderText(node: ASTNode): String {
-        return when(node.elementType) {
-            LuauTypes.FUNC_BODY -> "..."
-            LuauTypes.TABLE_CONSTRUCTOR -> (node.psi as LuauTableConstructor).fieldList.size.let { "{ $it fields }" } ?: ""
+        return when (node.elementType) {
+            LuauTypes.BLOCK -> "..."
+            LuauTypes.TABLE_CONSTRUCTOR -> (node.psi as LuauTableConstructor).fieldList.size.let { "{ $it fields }" }
             LuauTypes.BLOCK_COMMENT -> "--[[ ... ]]"
             else -> "..."
         }
