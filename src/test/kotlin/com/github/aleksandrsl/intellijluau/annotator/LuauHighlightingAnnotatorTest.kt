cd812f787ae59c20bc00ab2fc111de102dd9437a
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-04-15 09:43:37 +0200

    fix: stdlib references resolution
    Plethora of fixes
    1. Not all declarations can be used as is
    2. There are duplicating names across different declaration types
    3. Enums are now handled inside resolution plus the whole chain can be handled, keeping resolution in one place.
    4. Add tests and for them, I had to add a test implementation of Stdlib service.
    5. Correct highlight so only references are highlighted not all elements

---
+++ src/test/kotlin/com/github/aleksandrsl/intellijluau/annotator/LuauHighlightingAnnotatorTest.kt
@@ -0,0 +1,28 @@
+/*
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package com.github.aleksandrsl.intellijluau.annotator
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+
+const val baseTestDataPath = "src/test/testData/annotator"
+
+class LuauHighlightingAnnotatorTest : BasePlatformTestCase() {
+
+    override fun getTestDataPath(): String {
+        return baseTestDataPath
+    }
+
+    fun `test type parameters`() {
+        myFixture.configureByFile("typeParameters.luau")
+        myFixture.checkHighlighting(false, true, true, false)
+    }
+
+    fun `test stdlib`() {
+        myFixture.configureByFile("stdlib.luau")
+        myFixture.checkHighlighting(false, true, true, false)
+    }
+}
