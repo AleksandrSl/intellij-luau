61388da217f073313a8e81a64d1cef3e23a06f4c
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-08-04 23:27:01 +0200

    feat: sync with the plugin template 2.2.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.name = "intellij-luau"
 
 // They added this back in 2.0.2, but I have no idea why.
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.00.0"
 }
