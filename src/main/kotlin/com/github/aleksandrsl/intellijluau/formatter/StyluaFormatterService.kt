9441a7a4db455b6bce587aca518834814d3abaff
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-08-24 23:16:44 +0200

    feat: restructure formatting for clearer errors
    I think now I have better cancelling support, plus the errors shown if foreman stuff is broken or there are other problems with stylua are more descriptive

--- src/main/kotlin/com/github/aleksandrsl/intellijluau/formatter/StyluaFormatterService.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijluau/formatter/StyluaFormatterService.kt
@@ -3,19 +3,22 @@ package com.github.aleksandrsl.intellijluau.formatter
 import com.github.aleksandrsl.intellijluau.LuauNotifications
 import com.github.aleksandrsl.intellijluau.psi.LuauFile
 import com.github.aleksandrsl.intellijluau.settings.ProjectSettingsState
+import com.github.aleksandrsl.intellijluau.tools.StyLuaCli.FormatResult
 import com.github.aleksandrsl.intellijluau.tools.ToolchainResolver
 import com.intellij.codeInsight.actions.ReformatCodeProcessor
-import com.intellij.execution.ExecutionException
-import com.intellij.execution.process.CapturingProcessAdapter
-import com.intellij.execution.process.ProcessEvent
 import com.intellij.formatting.service.AsyncDocumentFormattingService
 import com.intellij.formatting.service.AsyncFormattingRequest
 import com.intellij.formatting.service.FormattingService
 import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.psi.PsiFile
 import com.intellij.psi.formatter.FormatterUtil
-import java.io.File
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import java.util.*
+
+private val LOG = logger<StyluaFormatterService>()
 
 class StyluaFormatterService : AsyncDocumentFormattingService() {
     override fun getFeatures(): Set<FormattingService.Feature> = FEATURES
@@ -34,40 +37,40 @@ class StyluaFormatterService : AsyncDocumentFormattingService() {
         val settings = ProjectSettingsState.getInstance(project)
         // This is a hack, maybe implementing LanguageFormattingRestriction is better,
         if (settings.disableBuiltinFormatter) return null
-        val stylua = ToolchainResolver.resolveStylua(project) ?: return null
 
-        try {
-            val handler = stylua.createOsProcessHandler(project, file)
+        // Has been heavily inspired by the https://github.com/JetBrains/intellij-community/blob/idea/243.26053.27/plugins/sh/core/src/com/intellij/sh/formatter/ShExternalFormatter.java
+        // Now it's inspired https://github.com/JetBrains/intellij-plugins/blob/c9ee79f92779c8f3c3f680961d19ebf11d5e1b93/terraform/src/org/intellij/terraform/hcl/formatter/TfAsyncFormattingService.kt
+        return object : FormattingTask {
+            private var job: Job? = null
 
-            return object : FormattingTask {
-                override fun run() {
-                    handler.processInput.use { it.write(request.documentText.toByteArray()) }
-                    // Heavy inspired by the https://github.com/JetBrains/intellij-community/blob/idea/243.26053.27/plugins/sh/core/src/com/intellij/sh/formatter/ShExternalFormatter.java
-                    handler.addProcessListener(object : CapturingProcessAdapter() {
-                        override fun processTerminated(event: ProcessEvent) {
-                            val exitCode = event.exitCode
-                            if (exitCode == 0) {
-                                request.onTextReady(output.stdout)
-                            } else {
-                                request.onError("Failed to format", output.stderr)
+            @Suppress("UnstableApiUsage")
+            override fun run() {
+                try {
+                    runBlockingCancellable {
+                        val stylua = ToolchainResolver.resolveStylua(project)
+                            ?: return@runBlockingCancellable request.onTextReady(request.documentText)
+                        job = launch {
+                            val result = stylua.runFormatProcess(project, file, request.documentText.toByteArray())
+                            when (result) {
+                                is FormatResult.Success -> request.onTextReady(result.content)
+                                is FormatResult.StyluaError -> request.onError("Failed to format", result.msg)
                             }
                         }
-                    })
-                    handler.startNotify()
-                }
-
-                override fun cancel(): Boolean {
-                    handler.destroyProcess()
-                    return true
-                }
-
-                override fun isRunUnderProgress(): Boolean {
-                    return true
+                    }
+                } catch (exception: Exception) {
+                    LOG.warn(exception)
+                    request.onError("Failed to format", exception.localizedMessage)
                 }
             }
-        } catch (e: ExecutionException) {
-            request.onError("Failed to format", e.message ?: "Unknown error")
-            return null
+
+            override fun cancel(): Boolean {
+                job?.cancel()
+                return true
+            }
+
+            override fun isRunUnderProgress(): Boolean {
+                return true
+            }
         }
     }
 
