5f00a16e05657fee930a24def53ede4637267fb0
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-09-18 14:01:56 +0200

    fix: creating all files as luau

--- src/main/kotlin/com/github/aleksandrsl/intellijluau/template/LuauCreateFromTemplateHandler.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijluau/template/LuauCreateFromTemplateHandler.kt
@@ -1,5 +1,6 @@
 package com.github.aleksandrsl.intellijluau.template
 
+import com.github.aleksandrsl.intellijluau.LuauFileType
 import com.github.aleksandrsl.intellijluau.settings.ProjectSettingsState
 import com.intellij.ide.fileTemplates.DefaultCreateFromTemplateHandler
 import com.intellij.ide.fileTemplates.FileTemplate
@@ -16,6 +17,9 @@ class LuauCreateFromTemplateHandler : DefaultCreateFromTemplateHandler() {
         templateText: String,
         props: Map<String?, Any?>
     ): PsiElement {
+        if (!template.isTemplateOfType(LuauFileType)) {
+            return super.createFromTemplate(project, directory, fileName, template, templateText, props)
+        }
         // I want a different extension based on the settings (Legacy projects may use .lua extension).
         // Having a separate template is weird given that I may have different templates, but the content is the same. So the best way is to use the correct extension.
         // I tried cloning a template and setting the correct extension for it, but it doesn't work, since the value is coming from an internal object I don't have access to.
@@ -30,6 +34,9 @@ class LuauCreateFromTemplateHandler : DefaultCreateFromTemplateHandler() {
 
     // See comment in createFromTemplate
     override fun checkAppendExtension(fileName: String?, template: FileTemplate): String? {
+        if (!template.isTemplateOfType(LuauFileType)) {
+            return super.checkAppendExtension(fileName, template)
+        }
         return fileName
     }
 }
