d7d55121940b2bdd18be7a0705725cc1291e2504
Aleksandr Slepchenkov <sl.aleksandr28@gmail.com>
2025-11-09 17:45:33 +0100

    fix: a quick one for "a colon should be placed outside quotation marks" in the new grammar checks
    The text for check in the literals was including the quotes. I tried different combinations of removingIndents and removingLineSuffixes, but they seem to fail removing the ending quote.

--- src/main/kotlin/com/github/aleksandrsl/intellijluau/spellcheck/LuauTextExtractor.kt
+++ src/main/kotlin/com/github/aleksandrsl/intellijluau/spellcheck/LuauTextExtractor.kt
@@ -6,6 +6,7 @@ import com.intellij.grazie.text.TextContent
 import com.intellij.grazie.text.TextContentBuilder
 import com.intellij.grazie.text.TextExtractor
 import com.intellij.grazie.utils.getNotSoDistantSimilarSiblings
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiUtilCore
 
@@ -14,8 +15,13 @@ class LuauTextExtractor : TextExtractor() {
     override fun buildTextContent(root: PsiElement, allowedDomains: MutableSet<TextContent.TextDomain>): TextContent? {
         val elementType = PsiUtilCore.getElementType(root) ?: return null
 
-        if (TextContent.TextDomain.LITERALS in allowedDomains && LuauTokenSets.STRINGS.contains(elementType)) {
-            return TextContentBuilder.FromPsi.build(root, TextContent.TextDomain.LITERALS)
+        if (TextContent.TextDomain.LITERALS in allowedDomains && elementType == LuauTypes.STRING) {
+            return TextContentBuilder.FromPsi
+                // I tried using indents and suffix removal, but it seems to ignore the trailing symbols.
+                // Manual removal of quotes is not ideal.
+                // Since it relies on the string being complete, which is fine, I think.
+                // And also ignores multiline strings.
+                .build(root, TextContent.TextDomain.LITERALS, TextRange(1, root.textLength - 1))
         }
 
         if (TextContent.TextDomain.DOCUMENTATION in allowedDomains && (elementType == LuauTypes.DOC_COMMENT || elementType == LuauTypes.DOC_BLOCK_COMMENT)) {
